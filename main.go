86d1d4bf0dcf9ec9ea84c94fee3d8966189d60bf
Søren Fuglede Jørgensen <s@fuglede.dk>
2016-03-27 19:48:24 +0200

    Load templates on initialisation

--- main.go
+++ main.go
@@ -4,29 +4,43 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
-func contentHandler(t *template.Template, w http.ResponseWriter, r *http.Request) {
-	t.ParseFiles("tmpl/base.html")
-	t.ExecuteTemplate(w, "base", nil)
-	t.Execute(w, nil)
+// We store all templates on first launch for efficiency.
+var templates map[string]*template.Template
+
+func initializeTemplates() {
+    if templates == nil {
+        templates = make(map[string]*template.Template)
+    }
+    templateFiles, err := filepath.Glob("tmpl/*.html")
+    if err != nil {
+        log.Fatal(err)
+    }
+    for _, t := range templateFiles {
+    	if t != "tmpl/base.html" {
+        	templates[t] = template.Must(template.ParseFiles("tmpl/base.html", t))
+    	}
+    }
+}
+
+// renderContent parses the content (given as a template) and puts it into our base template. 
+func renderContent(t string, w http.ResponseWriter, data interface{}) {
+	templates[t].ExecuteTemplate(w, "base", data)
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("tmpl/about.html")
-	contentHandler(t, w, r)
+	renderContent("tmpl/about.html", w, nil)
 }
 
 func frontPageHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("tmpl/base.html",  "tmpl/frontpage.html")
-	news := readNews()
-	err := t.ExecuteTemplate(w, "base", &news)
-	if err != nil {
-		log.Fatal(err)
-	}
+	renderContent("tmpl/frontpage.html", w, readNews())
 }
 
 func main() {
+	initializeTemplates()
+	
 	staticHandler := http.FileServer(http.Dir("tmpl"))
 	http.Handle("/css/", staticHandler)
 	http.Handle("/font/", staticHandler)
