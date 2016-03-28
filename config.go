fded7ca6f2aaa2904d1e5dc3fb7800cc29cc398b
Søren Fuglede Jørgensen <s@fuglede.dk>
2016-03-28 14:42:11 +0200

    Add configuration file for database connections

---
+++ config.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+
+type configType struct {
+	DbConnection	string	`json:"dbConnection"`
+}
+
+var config configType
+
+// readConfig reads the current configuration if it has not already been read.
+func readConfig() {
+	configFile, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Fatal("no config file found. Create one as config.json")
+	}
+	err = json.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Fatal("Could not read config file.")
+	}
+	return
+}
